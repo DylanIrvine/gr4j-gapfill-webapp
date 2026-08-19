92e782e8f98570fd89052a1b6969c55580abcf95
Dylan <dylan.irvine@cdu.edu.au>
2026-08-19 09:41:16 +0930

    Update app.py
    Further progress on main app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import numpy as np
+
+from core.gapfill import identify_gaps
 
 st.title('GR4J Gap Filling Tool')
 
-st.write('Application under development.')
+example = [1, 2, np.nan, np.nan, 5]
+
+gaps = identify_gaps(example)
+
+st.write('Gap detection test')
+st.write(gaps)
