20a8385befecc0896a8ea8111a4828041044f071
Dylan <dylan.irvine@cdu.edu.au>
2026-08-27 14:18:02 +0930

    Update app.py

--- app.py
+++ app.py
@@ -495,8 +495,8 @@ def build_metadata_df(cal, gap_method, n_missing, n_clipped, ensemble_units, she
 
 # %% header
 st.title('HYDROSTITCH')
-st.subheader(st.markdown("<u>HYDRO</u>logical <u>S</u>ignatures, <u>T</u>ime-<u>S</u>eries <u>I</u>nfilling and",
-"<u>T</u>ools for <u>C</u>atchment <u>H</u>ydrology"))
+st.subheader("**HYDRO**logical **S**ignatures, **T**ime-**S**eries **I**nfilling and",
+"**T**ools for **C**atchment **H**ydrology")
 st.write('Dylan Irvine, Charles Darwin University.\n')
 st.write(
     'The GR models (Modèle du Génie Rural à N paramètres Journalier) are simple, lumped '
