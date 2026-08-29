90f6c2a66235539f5618afcc3292a16f36a8be7f
Dylan <dylan.irvine@cdu.edu.au>
2026-08-30 06:42:20 +0930

    Update app.py

--- app.py
+++ app.py
@@ -503,7 +503,7 @@ with head_text:
     st.write('Dylan Irvine, Charles Darwin University.\n')
 
 with head_logo:
-    st.image('docs/HS_Logo.png', width='stretch', vertical_alignment='center')
+    st.image('docs/HS_Logo.png', width='stretch')
 
 st.write(
     'The GR models (Modèle du Génie Rural à N paramètres Journalier) are simple, lumped '
